d5d933ca1f6aa8f52ae46dd4e0ac18119af688c8
GarifullinII <ildaridrisovich@gmail.com>
2021-12-15 22:31:40 +0400

    added pages user_info_page

---
+++ lib/pages/user_info_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class UserInfoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('user info'),
+        centerTitle: true,
+      ),
+      body: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(
+                'Name',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              subtitle: Text('Story'),
+              leading: Icon(
+                Icons.person,
+                color: Colors.black,
+              ),
+              trailing: Text('Country'),
+            ),
+            ListTile(
+              title: Text(
+                'Phone',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              leading: Icon(
+                Icons.phone,
+                color: Colors.black,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Email',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              leading: Icon(
+                Icons.phone,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
